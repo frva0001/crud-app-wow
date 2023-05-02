034749e11da06e2f8cf14de0587d5cb16e1b3659
Kimkold <Valdehannibal@hotmail.com>
2023-05-02 15:31:19 +0200

    removed console clutter

--- 2-get-and-show-characters.js
+++ 2-get-and-show-characters.js
@@ -7,16 +7,16 @@ console.log(characters);
 // fetch our character-data
 
 async function getCharacters() {
-   const response = await fetch(`${endpoint}/characters.json`);
-   const data = await response.json();
-   const characters = prepareData(data);
-   console.log(characters);
-   return characters;
+  const response = await fetch(`${endpoint}/characters.json`);
+  const data = await response.json();
+  const characters = prepareData(data);
+  console.log(characters);
+  return characters;
 }
 
 //Når du opdaterer denne function, kan du så copy-paste den ind i filter.js?
 function showCharacter(character) {
-   const html = /* html */ `
+  const html = /* html */ `
         <article class="character-grid-item">
             <h3>${character.characterName}<h3>
             <h3>${character.level}<h3>
@@ -27,85 +27,88 @@ function showCharacter(character) {
         </article>
     `;
 
-   function isActive(character) {
-      console.log(character);
-      let html1 = "";
-      if (character.activities.leveling) {
-         html1 += `<p>Leveling and BFA Dungeons</p>`;
-      }
-      if (character.activities.questing) {
-         html1 += `<p>Questing</p>`;
-      }
-      if (character.activities.mythicPlus1) {
-         html1 += `<p>Mythic Plus Key 0-10</p>`;
-      }
-      if (character.activities.mythicPlus2) {
-         html1 += `<p>Mythic Plus Key 10-15</p>`;
-      }
-      if (character.activities.mythicPlus3) {
-         html1 += `<p>Mythic Plus Key 15-20</p>`;
-      }
-      if (character.activities.mythicPlus4) {
-         html1 += `<p>Mythic Plus Key 20 and above</p>`;
-      }
-      if (character.activities.raid1) {
-         html1 += `<p>Normal Vault of the Incarnates</p>`;
-      }
-      if (character.activities.raid2) {
-         html1 += `<p>Heroic Vault of the Incarnates</p>`;
-      }
-      if (character.activities.raid3) {
-         html1 += `<p>Mythic Vault of the Incarnates</p>`;
-      }
-      if (character.activities.pvp1) {
-         html1 += `<p>Battelgrounds 10v10, 15v15</p>`;
-      }
-      if (character.activities.pvp2) {
-         html1 += `<p>Arenas 2v2, 3v3</p>`;
-      }
-      if (character.activities.transmogsAndMounts) {
-         html1 += `<p>Transmog hunting and Mount farming</p>`;
-      }
+  function isActive(character) {
+    let html1 = "";
+    if (character.activities.leveling) {
+      html1 += `<p>Leveling and BFA Dungeons</p>`;
+    }
+    if (character.activities.questing) {
+      html1 += `<p>Questing</p>`;
+    }
+    if (character.activities.mythicPlus1) {
+      html1 += `<p>Mythic Plus Key 0-10</p>`;
+    }
+    if (character.activities.mythicPlus2) {
+      html1 += `<p>Mythic Plus Key 10-15</p>`;
+    }
+    if (character.activities.mythicPlus3) {
+      html1 += `<p>Mythic Plus Key 15-20</p>`;
+    }
+    if (character.activities.mythicPlus4) {
+      html1 += `<p>Mythic Plus Key 20 and above</p>`;
+    }
+    if (character.activities.raid1) {
+      html1 += `<p>Normal Vault of the Incarnates</p>`;
+    }
+    if (character.activities.raid2) {
+      html1 += `<p>Heroic Vault of the Incarnates</p>`;
+    }
+    if (character.activities.raid3) {
+      html1 += `<p>Mythic Vault of the Incarnates</p>`;
+    }
+    if (character.activities.pvp1) {
+      html1 += `<p>Battelgrounds 10v10, 15v15</p>`;
+    }
+    if (character.activities.pvp2) {
+      html1 += `<p>Arenas 2v2, 3v3</p>`;
+    }
+    if (character.activities.transmogsAndMounts) {
+      html1 += `<p>Transmog hunting and Mount farming</p>`;
+    }
 
-      return html1;
-   }
-   document.querySelector("#characters").insertAdjacentHTML("beforeend", html);
+    return html1;
+  }
+  document.querySelector("#characters").insertAdjacentHTML("beforeend", html);
 
-   //event listeners to btns
-   document.querySelector("#characters article:last-child .btn-update-character").addEventListener("click", () => updateClicked(character));
-   document.querySelector("#characters article:last-child .btn-delete-character").addEventListener("click", () => deleteClicked(character));
+  //event listeners to btns
+  document
+    .querySelector("#characters article:last-child .btn-update-character")
+    .addEventListener("click", () => updateClicked(character));
+  document
+    .querySelector("#characters article:last-child .btn-delete-character")
+    .addEventListener("click", () => deleteClicked(character));
 
-   // function updateClicked() {
-   //   console.log("test");
-   //   const update = document.querySelector("#form-update-character");
+  // function updateClicked() {
+  //   console.log("test");
+  //   const update = document.querySelector("#form-update-character");
 
-   //   update.updateName.value = character.characterName;
-   //   update.updateRace.value = character.race;
-   //   update.updateFaction.value = character.title;
-   //   update.updateClass.value = character.class;
-   //   update.updateSpecialization.value = character.specialization;
-   //   update.updateRole.value = character.role;
-   //   update.updateItemLevel.value = character.itemLevel;
-   //   update.updateLevel.value = character.level;
-   //   update.updateCountry.value = character.country;
-   //   update.updateBattletag.value = character.battletag;
+  //   update.updateName.value = character.characterName;
+  //   update.updateRace.value = character.race;
+  //   update.updateFaction.value = character.title;
+  //   update.updateClass.value = character.class;
+  //   update.updateSpecialization.value = character.specialization;
+  //   update.updateRole.value = character.role;
+  //   update.updateItemLevel.value = character.itemLevel;
+  //   update.updateLevel.value = character.level;
+  //   update.updateCountry.value = character.country;
+  //   update.updateBattletag.value = character.battletag;
 
-   //   update.setAttribute("data-id", character.id);
-   //   document.querySelector("#dialog-update-character").showModal();
-   // }
+  //   update.setAttribute("data-id", character.id);
+  //   document.querySelector("#dialog-update-character").showModal();
+  // }
 
-   //  function deleteClicked() {
-   //     const deletePost = document.querySelector("#form-delete-post");
+  //  function deleteClicked() {
+  //     const deletePost = document.querySelector("#form-delete-post");
 
-   //     deletePost.setAttribute("data-id", character.id);
-   //     document.querySelector("#dialog-delete-post").showModal();
-   //  }
+  //     deletePost.setAttribute("data-id", character.id);
+  //     document.querySelector("#dialog-delete-post").showModal();
+  //  }
 }
 
 function showCharacters(allCharacters) {
-   document.querySelector("#characters").innerHTML = "";
+  document.querySelector("#characters").innerHTML = "";
 
-   for (const character of allCharacters) {
-      showCharacter(character);
-   }
+  for (const character of allCharacters) {
+    showCharacter(character);
+  }
 }
